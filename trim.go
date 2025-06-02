21309e50f10a9329c01288dbaba55686694068ee
Chris Caldwell <153000660+chrisbcaldwell@users.noreply.github.com>
2025-06-01 20:38:28 -0700

    Create trim.go

---
+++ trim.go
@@ -0,0 +1,12 @@
+package trim
+
+func Trim() {
+    // Trim returns a copy of x with the specified proportion of values removed from the lower and upper ends
+}
+
+func TrimmedMean() float64{
+    // TrimmedMean returns the arithmetic mean of x, with the specified proportion of values removed
+    // from the lower and upper ends
+    return 51.0
+}
+
