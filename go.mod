c23ed613080062dec56b450c867aba17383f026d
Chris Caldwell <chriscaldwell2026@u.northwestern.edu>
2025-06-04 20:40:08 -0700

    finished trim.go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module trim
+
+go 1.23.2
+
+require golang.org/x/exp v0.0.0-20250531010427-b6e5de432a8b
---
+++ trim.go
@@ -0,0 +1,49 @@
+package trim
+
+import (
+	"slices"
+
+	"golang.org/x/exp/constraints"
+)
+
+func Trim[T number](slice []T, q quantiles) []T {
+	slices.Sort(slice)
+	length := len(slice)
+	if length == 0 {
+		return []T{}
+	}
+
+	// set default quantile behavior
+	if q.High == 0 {
+		q.High = q.Low
+	}
+
+	lowI := int(q.Low * float64(length))
+	highI := length - int(q.High*float64(length)) - 1
+	return slice[lowI:highI]
+
+}
+
+func TrimmedMean[T number](slice []T, q quantiles) T {
+	return mean(Trim(slice, q))
+}
+
+type quantiles struct {
+	Low  float64 // default will be 0 automatically
+	High float64 // default will be Low first, then 0
+}
+
+type number interface {
+	constraints.Integer | constraints.Float
+}
+
+func mean[T number](slice []T) T {
+	sum := slice[0]
+	if len(slice) == 1 {
+		return sum
+	}
+	for i := 1; i < len(slice); i++ {
+		sum += slice[i]
+	}
+	return sum / T(len(slice))
+}
