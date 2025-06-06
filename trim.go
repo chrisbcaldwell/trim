38e0c348b56a8c8af09f867d8863e2bef0c2d7f0
Chris Caldwell <chriscaldwell2026@u.northwestern.edu>
2025-06-06 07:10:07 -0700

    updated Trim comment

--- trim.go
+++ trim.go
@@ -8,7 +8,7 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-// Trim cuts a slice of numbers by the proportions in q
+// Trim cuts a sorted slice of numbers by the proportions in q
 func Trim[T number](slice []T, q quantiles) []T {
 	slices.Sort(slice)
 	length := len(slice)
